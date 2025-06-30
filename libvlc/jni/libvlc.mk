68cb62df7afc22978d4ccad4349d992dc1a290ce
Steve Lhomme <robux4@ycbcr.xyz>
2025-06-30 12:57:41 +0200

    buildsystem: ensure APP_STL is used when building libvlc.so
    It's only used if any of the sources to compile are C++.
    This is not the case as we only build libvlcjni-modules.c and libvlcjni-symbols.c.
    
    It links properly because some pkg-config files are forcing -lc++ when linking.
    
    We can give a hint to ndk-build that the code contains C++ code.

--- libvlc/jni/libvlc.mk
+++ libvlc/jni/libvlc.mk
@@ -23,6 +23,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE    := libvlc
 LOCAL_SRC_FILES := $(VLC_BUILD_DIR)/ndk/libvlcjni-modules.c \
 				   $(VLC_BUILD_DIR)/ndk/libvlcjni-symbols.c
+LOCAL_HAS_CPP := true
 LOCAL_LDFLAGS := -L$(VLC_CONTRIB)/lib
 LOCAL_LDLIBS := \
     $(VLC_MODULES) \
