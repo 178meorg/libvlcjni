82f21cdc8e122ce32d40c2709426a45c39343561
Nicolas Pomepuy <nicolas@videolabs.io>
2025-09-12 07:47:22 +0200

    Bump vlc hash and lib version

--- buildsystem/get-vlc.sh
+++ buildsystem/get-vlc.sh
@@ -43,7 +43,7 @@ check_patch_is_applied()
     fi
 }
 
-VLC_TESTED_HASH=b251b0a7e811bfa0b0550824a8e5e571947afd6c
+VLC_TESTED_HASH=0a0e4be968e130e91c6bca8015d6a8ed7fc5fae4
 VLC_REPOSITORY=https://code.videolan.org/videolan/vlc.git
 VLC_BRANCH=master
 
